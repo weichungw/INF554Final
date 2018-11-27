2761eb70986707ed94b837840f0e5aef929f2e40
your_name <your_email>
2018-11-27 13:12:04 -0800

    packing

--- src/app/danger-ratio/danger-ratio.component.ts
+++ src/app/danger-ratio/danger-ratio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { ConsoleReporter } from 'jasmine';
 
 export interface LeafNode {data:DangerBubble ,x:number, y:number}
 export interface DangerBubble{count:number, name: String, ratio:number}
@@ -76,9 +77,10 @@ export class DangerRatioComponent implements OnInit {
 
   initHierarchy(){
     var div = d3.select("#danger-control-div");
-    var c_width:number = 800;
+    var c_width:number =1400;
     var c_height:number = 800;
     var canvas = div.append("svg")
+      .attr("id","pack_layout")
       .attr('width',c_width)
       .attr('height',c_height);
 
@@ -89,11 +91,13 @@ export class DangerRatioComponent implements OnInit {
     var  g= canvas.append("g")
     
     // init required component
+
+    var color = d3.scaleSequential(d3.interpolateRdPu)
+      .domain([8, -1]);
+    var diameter = Math.min(width*0.9, height*0.9);
     var pack =d3.pack()
-      .size([width,height])
-      .padding(2);
-    var color = d3.scaleSequential(d3.interpolateGnBu)
-      .domain([-1, 5]);
+    .size([diameter,diameter]);
+    // .padding(2);
 
     var tooltip = d3.select("body").append("div")
       .attr('class',"tooltip-danger")
@@ -107,7 +111,22 @@ export class DangerRatioComponent implements OnInit {
       var root = d3.hierarchy(data)
         .sum(function(d){ return Math.sqrt(d['count']);})
         .sort(function(a,b){ return b.value-a.value;});
+      
+  
+      // const svg = canvas
+      // .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+      // .style("display", "block")
+      // .style("margin", "0 -14px")
+      // .style("width", "calc(100% + 28px)")
+      // .style("height", "auto")
+      // .style("background", color(0))
+      // .style("cursor", "pointer")
+      // .on("click", () => zoom(root));
+
       var nodes = pack(root).descendants();
+      const roott = pack(root)
+      let focus = pack(root);
+      let view
 
       var circles =g.selectAll(".bubble")
         .data(nodes)
@@ -116,10 +135,11 @@ export class DangerRatioComponent implements OnInit {
         .attr("cx",node=>node.x)
         .attr("cy",node=>node.y)
         .attr("r",node=>node.r)
-        .style("fill",function(d){return d.children? color(d.depth):"white";});
-
-      var leaves = g.selectAll(".leaf, .bubble")
-      .on("mouseenter", function(node:LeafNode){
+        .style("fill",function(d){return  color(d.depth)})
+        .on("click", d => focus !== d && (console.log("there"),zoom(d), d3.event.stopPropagation()));
+      
+        var leaves = g.selectAll(".leaf, .bubble")
+          .on("mouseenter", function(node:LeafNode){
           console.log(node)
           tooltip.html("name:"+node.data["name"]+"<br/>"
                   +"Ratio:"+node.data["ratio"])
@@ -135,25 +155,90 @@ export class DangerRatioComponent implements OnInit {
             .duration(500)
             .style("opacity",0);
         });
+
       var b_label = g.selectAll(".bubble-label")
         .data(nodes)
         .enter().append("text")
         .attr("class","bubble-label")
         .attr("text-anchor","middle")
         .attr("alignment-baseline","middle")
-        .attr("transform", node=>{
-          console.log(node)
+        .attr("y",node=>{
           if(node.children){
-            return "translate("+node.x+", "+(node.y-node.r*0.8)+")"
-          }else{
-            return "translate("+node.x+", "+(node.y)+")"
-          }
-        })
+            return  node.y-node.r*0.8}
+          else{
+              return node.y}})
+
+        .attr("x",node=>node.x)
+        // .attr("transform", node=>{
+        //   if(node.children){
+        //     return "translate("+node.x+", "+(node.y-node.r*0.8)+")"
+        //   }else{
+        //     return "translate("+node.x+", "+(node.y)+")"
+        //   }
+        // })
         .attr("pointer-events","none")
-      //  .style("fill-opacity",node=>node.children?0:1)
-      //.style("fill",node=>c2c.getColor(node.data['name']))
         .style("font-size","0.8em")
         .text(node=>node.data['name']);
+
+ 
+      
+        console.log(roott.x, roott.y)
+        zoom(roott)
+        // zoomTo([roott.x, roott.y, roott.r*2]);
+        view = [roott.x, roott.y, roott.r*2]
+        function zoomTo(v) {
+          console.log(roott.x, roott.y)
+          const k =diameter/v[2]-1;
+        
+          // v=[0,0,roott.r*2]
+          // k=a
+          console.log(k)
+          // const k2 
+          view = v;
+          console.log("where is d")
+          circles.attr("transform", d =>( console.log(d),`translate(${(d.x - v[0])*k},${(d.y- v[1])*k})`));
+          // b_label.attr("transform", d => `translate(${(d.x - v[0]) * (k)},${(d.y - v[1]) * (k)})`);
+          
+
+          b_label.attr("transform", d=>{
+          if(d.children){
+           return  `translate(${(d.x - v[0])*k},${(d.y- d.r*0.8-v[1])*k})`
+        
+          // { return 'translate( ${(d.x - v[0]) * (k)}, ${(d.y-d.r*0.8-v[1])*k})'
+          }else{
+            return  `translate(${(d.x - v[0])*k},${(d.y- d.r*0.8-v[1])*k})`
+            // return "translate( ${(d.x - v[0]) * (k)} , ${(d.y-v[1])*k}"+")"
+          }
+        })
+          // b_label.attr("transform", d =>( console.log(d),`translate(50,50)`));
+          // b_label.attr("transform", node=>{
+          //   if(node.children){
+          //     return "translate("+node.x+", "+(node.y-node.r*0.8)+")"
+          //   }else{
+          //     return "translate("+node.x+", "+(node.y)+")"
+          //   }
+          // }).style("transform", d =>( console.log(d),`translate(50,50)`));
+          // console.log("minus result ",d =>((d.x - v[0]),(d.y- v[1]),d),);
+          // circles.attr("transform", d => `translate((d.x - v[0])*k,(d.y - v[0])*k)`);
+          circles.attr("r", d => d.r * k+d.r );
+        }
+        function zoom(d) {
+          focus = d;
+          const transition = canvas.transition()
+              .duration(2000)
+              .tween("zoom", d => {
+                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+                return t =>(console.log(t,i(t)),zoomTo(i(t)));
+              });
+      
+          // b_label
+          //   .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+          //   .transition(transition)
+          //     .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+          //     .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+          //     .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+        }
+      
 
     });
 
